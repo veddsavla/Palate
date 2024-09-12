801ac5bc2de6e1b848094bc1e3ad146310d6f6a2
Vedansh Savla <savlavedansh@gmail.com>
2024-09-12 20:05:35 +0530

    navbar

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Navbar></Navbar>
+    </div>
+  );
+};
+
+export default App;
