c412b42e61861e4de80d3af999865dc03309dfde
Vedansh Savla <savlavedansh@gmail.com>
2024-09-13 19:09:45 +0530

    header

---
+++ frontend/src/components/Navbar/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favorite food here</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+          magnam qui quae libero, recusandae labore veniam totam neque, saepe
+          vel fugiat? Voluptates assumenda ad enim eaque quae quibusdam quod
+          mollitia.
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
