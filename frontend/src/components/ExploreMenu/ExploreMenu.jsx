2eff9ef6536f5ad674e147ab89e14fc3d97fb990
Vedansh Savla <savlavedansh@gmail.com>
2024-09-30 17:37:49 +0530

    explore-menu

---
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./ExploreMenu.css";
+import { menu_list } from "../../assets/assets";
+
+const ExploreMenu = () => {
+  return (
+    <div className="explore-menu" id="explore-menu">
+      <h1>Explore our menu</h1>
+      <p className="explore-menu-text">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis error
+        delectus eligendi nam ipsa beatae illo maiores veniam sunt corrupti
+        nihil, ab assumenda necessitatibus consequuntur? Voluptas illum et neque
+        debitis.
+      </p>
+      <div className="explore-menu-list">
+        {menu_list.map((item, index) => {
+          return (
+            <div key={index} className="explore-menu-list-item">
+              <img src={item.menu_image} alt="" />
+              <p>{item.menu_name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default ExploreMenu;
