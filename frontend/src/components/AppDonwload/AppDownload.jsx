8c1a6cf1fd41890e92aa03911957a1cc43bae77c
Vedansh Savla <savlavedansh@gmail.com>
2024-10-10 17:40:36 +0530

    login popup

--- frontend/src/components/AppDonwload/AppDownload.jsx
+++ frontend/src/components/AppDonwload/AppDownload.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import "./AppDownload.css";
 import { assets } from "../../assets/assets";
 
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
 
-const Navbar = () => {
+const Navbar = ({ setShowLogin }) => {
   const [menu, setMenu] = useState("home");
 
   return (
@@ -40,7 +40,7 @@ const Navbar = () => {
           <img src={assets.basket_icon} alt="" />
           <div className="dot"></div>
         </div>
-        <button>sign in</button>
+        <button onClick={() => setShowLogin(true)}>sign in</button>
       </div>
     </div>
   );
