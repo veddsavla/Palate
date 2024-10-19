4e0ac9c9012bf78eb417328eb245496366ff9a6f
Vedansh Savla <savlavedansh@gmail.com>
2024-10-19 06:58:34 +0530

    user auth

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,11 +2,66 @@ import userModel from "../models/userModel.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import validator from "validator";
+import "dotenv/config";
 
 // login user
 const loginUser = async (req, res) => {};
 
+// Function to create JWT token
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET);
+};
+
 // register user
-const registerUser = async (req, res) => {};
+const registerUser = async (req, res) => {
+  const { name, password, email } = req.body;
+  try {
+    // Check if the user already exists
+    const exists = await userModel.findOne({ email });
+    if (exists) {
+      return res.json({ success: false, message: "User already exists" });
+    }
+
+    // Validate the email format
+    if (!validator.isEmail(email)) {
+      return res.json({
+        success: false,
+        message: "Please enter a valid email",
+      });
+    }
+
+    // Check if the password is strong (minimum 8 characters)
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: "Please enter a strong password",
+      });
+    }
+
+    // Hashing the user's password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create a new user
+    const newUser = new userModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    // Save the user and generate a token
+    const user = await newUser.save();
+    const token = createToken(user._id);
+
+    // Return the token as a response
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    return res.json({
+      success: false,
+      message: "Error",
+    });
+  }
+};
 
 export { loginUser, registerUser };
